116cdc6658728ce2738a42d9b1b37419907973ff
Hannes Vogt <hannes@havogt.de>
2017-07-20 08:45:54 +0200

    remove serialbox

--- libs/serialbox/python/serialbox/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is released under terms of BSD license`
-# See LICENSE.txt for more information
-
-from .Visualizer import Visualizer
-from .Serializer import Serializer
-
--- libs/serialbox/src/utils/Logger.cpp
+++
@@ -1,7 +0,0 @@
-//This file is released under terms of BSD license`
-//See LICENSE.txt for more information
-
-#include "Logger.h"
-
-stella::detail::StellaOutput sout;
-
