dd666bfc7c7687a00c5aead995f5d3d3aa42aeff
lukasm91 <lukasm91@users.noreply.github.com>
2018-08-13 15:58:40 +0200

    Fix unittest after renaming enum constants (#30)
    Names of enum constants have changed and need to be renamed in this repo as well when updating the dycore to a new gridtools version.

--- unittest/helper_dycore.h
+++ unittest/helper_dycore.h
@@ -17,13 +17,13 @@ const int jPlusHaloSize = 3;
 namespace {
 
 #ifdef DYCORE_USE_GPU
-    using StorageTraitsType = gridtools::storage_traits< gridtools::enumtype::Cuda >;
+    using StorageTraitsType = gridtools::storage_traits< gridtools::platform::cuda >;
 #else
-    using StorageTraitsType = gridtools::storage_traits< gridtools::enumtype::Host >;
+    using StorageTraitsType = gridtools::storage_traits< gridtools::platform::x86 >;
 #endif
 
     using IJHaloType = gridtools::halo< iMinusHaloSize, jMinusHaloSize, 0 >;
-} // end namespace internal
+} // namespace
 
 using IJKStorageInfoType = ::StorageTraitsType::storage_info_t< 0, 3, ::IJHaloType >;
 using IJKRealField = ::StorageTraitsType::data_store_t< Real, IJKStorageInfoType >;
